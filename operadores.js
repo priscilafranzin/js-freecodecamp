f0085bda524166e5cf3b7d6491d25be4e52fcc70
Priscila <anaandrade7464@gmail.com>
2023-03-16 19:10:48 -0300

    operadores e string

--- operadores.js
+++ operadores.js
@@ -1,1 +1,86 @@
 const sum = 10 + 10;
+a = a + 1;
+b = b + 5;
+c = c + " String!";
+//camelCase
+// Variable declarations
+var studlyCapVar;
+var properCamelCase;
+var titleCaseOver;
+
+// Variable assignments
+studlyCapVar = 10;
+properCamelCase = "A String";
+titleCaseOver = 9000;
+
+{
+    // let x const
+    const FCC = "freeCodeCamp"; // Change this line
+    let fact = "is cool!"; // Change this line
+    fact = "is awesome!";
+    console.log(FCC, fact); // Change this line
+    const difference = 45 - 33
+    const product = 8 * 10;
+    const quotient = 66 / 33;
+}
+
+{
+    let myVar = 87;
+
+// Only change code below this line
+    myVar++;
+}
+let myVar = 11;
+
+// Only change code below this line
+myVar--;
+const ourDecimal = 5.7;
+
+// Only change code below this line
+const myDecimal = 1.2
+const product = 2.0 * 2.5;
+const quotient = 4.4 / 2.0; // Change this line
+const remainder = 11 % 3 ;
+{
+    let a = 3;
+    let b = 17;
+    let c = 12;
+
+// Only change code below this line
+    sa = a += 12;
+    b = b += 9;
+    c = c += 7;
+}
+
+{
+    let a = 11;
+    let b = 9;
+    let c = 3;
+
+// Only change code below this line
+    a = a -= 6;
+    b = b -= 15;
+    c = c -= 1;
+}
+
+{
+    let a = 5;
+    let b = 12;
+    let c = 4.6;
+
+// Only change code below this line
+    a = a *= 5;
+    b = b *= 3;
+    c = c *= 10;
+}
+
+{
+    let a = 48;
+    let b = 108;
+    let c = 33;
+
+// Only change code below this line
+    a = a /= 12;
+    b = b /= 4;
+    c = c /= 11;
+}
--- vaiaveis.js
+++ vaiaveis.js
@@ -12,22 +12,3 @@ var b = 10;
 var c = 'I am a';
 // Only change code above this line
 
-a = a + 1;
-b = b + 5;
-c = c + " String!";
-//camelCase
-// Variable declarations
-var studlyCapVar;
-var properCamelCase;
-var titleCaseOver;
-
-// Variable assignments
-studlyCapVar = 10;
-properCamelCase = "A String";
-titleCaseOver = 9000;
-
-// let x const
-const FCC = "freeCodeCamp"; // Change this line
-let fact = "is cool!"; // Change this line
-fact = "is awesome!";
-console.log(FCC, fact); // Change this line
