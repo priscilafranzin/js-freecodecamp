f9f3ce7be8d4a58e71aaa7eedb8576cf0fe41374
Priscila <anaandrade7464@gmail.com>
2023-04-05 18:37:01 -0300

    if e else e switch

--- função.js
+++ função.js
@@ -252,3 +252,127 @@ testLessThan(10);
   
   testLogicalOr(15);
 }
+{
+  function testElse(val) {
+  let result = "";
+  // Only change code below this line
+
+  if (val > 5) {
+    result = "Bigger than 5";
+  }
+
+  else {
+    result = "5 or Smaller";
+  }
+
+  // Only change code above this line
+  return result;
+}
+
+testElse(4);
+}
+{
+  function testElseIf(val) {
+  if (val > 10) {
+    return "Greater than 10";
+  }
+
+ else if (val < 5) {
+    return "Smaller than 5";
+  }
+else{
+  return "Between 5 and 10";
+}
+}
+
+
+testElseIf(7);
+}
+{
+  function orderMyLogic(val) {
+  if (val < 5) {
+    return "Less than 5";
+  } else if (val < 10) {
+    return "Less than 10";
+  } else {
+    return "Greater than or equal to 10";
+  }
+}
+
+orderMyLogic(7);
+}
+{
+  function testSize(num) {
+  // Only change code below this line
+if (num < 5) {
+  return 'Tiny'
+}
+ else if (num < 10) {
+   return 'Small'
+ }
+ else if ( num < 15) {
+return 'Medium'
+ }
+ else if (num < 20) {
+   return 'Large'
+ }
+ else if ( num >= 20) {
+   return 'Huge'
+ }
+
+  return "Change Me";
+}
+}
+{
+  const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+  // Only change code below this line
+  if ( strokes === 1) {
+    return names[0]
+  } else if ( strokes <= par -2) {
+    return names[1]
+  } else if (strokes === par -1) {
+    return names[2]
+  } else if (strokes === par) {
+    return names[3]
+  } else if (strokes === par +1) {
+    return names[4]
+  } else if (strokes === par +2) {
+    return names[5]
+  } else if (strokes >= par +3) {
+    return names[6]
+  }
+
+  return "Change Me";
+  // Only change code above this line
+}
+
+golfScore(5, 4);
+}
+{
+  function caseInSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+switch (val) {
+  case 1 :
+    answer = 'alpha'
+    break;
+  case 2 :
+    answer = 'beta'
+    break;
+  case 3 : 
+    answer = 'gamma'
+    break;
+  case 4 :
+    answer = 'delta'
+    break ;
+}
+
+
+  // Only change code above this line
+  return answer;
+}
+
+caseInSwitch(1);
+}
