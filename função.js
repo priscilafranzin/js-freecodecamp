0582482b4f33c32898692104e2d51983d160edb0
Priscila <anaandrade7464@gmail.com>
2023-04-10 20:36:13 -0300

    condicionais switch, if, else

--- função.js
+++ função.js
@@ -376,3 +376,152 @@ switch (val) {
 
 caseInSwitch(1);
 }
+{
+  function switchOfStuff(val) {
+  let answer = "";
+  // Only change code below this line
+switch (val) {
+  case 'a' :
+    answer = 'apple';
+    break;
+  case 'b' :
+    answer = 'bird';
+    break;
+  case 'c' :
+    answer = 'cat';
+    break;
+  default:
+    answer = 'stuff'
+}
+{
+  function sequentialSizes(val) {
+  let answer = "";
+  // Only change code below this line
+switch (val) {
+  case 1:
+  case 2:
+  case 3:
+    answer = 'Low';
+    break;
+  case 4:
+  case 5:
+  case 6:
+    answer = 'Mid'
+    break;
+  case 7:
+  case 8:
+  case 9:
+    answer = 'High'
+    break;
+}
+
+
+  // Only change code above this line
+  return answer;
+}
+
+sequentialSizes(1);
+}
+
+
+  // Only change code above this line
+  return answer;
+}
+
+switchOfStuff(1);
+}
+{
+  function chainToSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+switch (val) {
+  case 'bob':
+    answer = 'Marley';
+    break;
+  case 42:
+    answer = 'The Answer';
+    break;
+  case 1:
+    answer = "There is no #1";
+    break;
+  case 99:
+    answer = "Missed me by this much!";
+    break;
+  case 7:
+    answer = 'Ate Nine';
+    break;
+
+}
+ 
+ 
+  // Only change code above this line
+  return answer;
+}
+
+chainToSwitch(7);
+}
+{
+  function isLess(a, b) {
+  // Only change code below this line
+  return a < b;
+  
+  // Only change code above this line
+}
+
+isLess(10, 15);
+}
+{
+  // Setup
+function abTest(a, b) {
+  // Only change code below this line
+
+if (a < 0 || b < 0 ) {
+  return undefined
+}
+
+  // Only change code above this line
+
+  return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+
+abTest(2,2);
+}
+{
+  //contagem atual
+let count = 0;
+
+function cc(card) {
+  // Only change code below this line
+  
+// condicionais card
+if ( card >= 2 && card <= 6) {
+  count += 1
+} 
+if (card === 10) {
+count -= 1
+}
+if ( card === 'J'){
+  count -= 1
+}
+if ( card === 'Q'){
+  count -= 1
+}
+if ( card === 'K'){
+  count -= 1
+}
+if ( card === 'A'){
+  count -= 1
+}
+
+if ( count > 0 ){
+  return count +  ' ' + 'Bet'
+}
+
+
+
+  return count + ' ' + "Hold";
+  // Only change code above this line
+}
+
+console.log(cc(2)); cc(3); cc(8); cc('K'); cc('A');
+}
