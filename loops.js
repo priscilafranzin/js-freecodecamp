6d7b2390f5ea5a2c2629e3f28393b8ea9ee5b579
Priscila <anaandrade7464@gmail.com>
2023-04-28 18:20:12 -0300

    loops de interação

---
+++ loops.js
@@ -0,0 +1,43 @@
+{const myArray = [];
+let i = 5;
+
+while (i >= 0) {
+  myArray.push(i);
+  console.log(myArray)
+  i--;}
+}
+{
+    const myArray = [];
+for (let i = 1; i <= 5; i++){
+  myArray.push(i)
+}
+console.log(myArray)
+}
+{
+    const myArray = [];
+for (let i = 1; i<= 9 ;i += 2){
+myArray.push(i)
+}
+console.log(myArray)
+}
+{
+    const myArray = [];
+for ( let i = 9; i > 0; i-= 2){
+  myArray.push(i)
+}
+console.log(myArray)
+}
+{
+    let x = 0
+    console.log(x)
+    x + 3
+    console.log(x)
+}
+{
+    const myArr = [2, 3, 4, 5, 6];
+let total = 0
+for ( let i = 0; i < myArr.length; i++){
+  total += myArr[i]
+  console.log(total)
+}
+}
