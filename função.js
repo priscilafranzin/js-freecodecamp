b8e63c33f05c54d5b67f7f36ee59ddd679bad747
Priscila <anaandrade7464@gmail.com>
2023-05-09 19:09:00 -0300

    operador ternario

--- função.js
+++ função.js
@@ -584,3 +584,35 @@ console.log(sum([2,3,4],1))
   
   }
 }
+{
+  function convertToInteger(str) {
+return parseInt(str);
+
+}
+
+convertToInteger("56");
+}
+{
+  function convertToInteger(str) {
+return parseInt(str , 2)
+}
+
+convertToInteger("10011");
+}
+// operador ternario
+{
+  function checkEqual(a, b) {
+return a === b ? 'Equal' : 'Not Equal';
+}
+
+checkEqual(1, 2);
+}
+{
+  function checkSign(num) {
+return (num > 0) ? 'positive'
+: (num < 0 ) ? 'negative'
+:"zero";
+}
+
+console.log(checkSign(0));
+}
