2bb9e5937ffce3863111ea644f8d52929d3879ba
Priscila <anaandrade7464@gmail.com>
2023-05-10 19:17:48 -0300

    freeze object

--- fundamentos/função.js
+++ fundamentos/função.js
@@ -643,3 +643,47 @@ console.log(checkSign(0));
   };
 }
 }
+//ES6>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+{
+  function checkScope() {
+    var i = 'function scope';
+    if (true) {
+      i = 'block scope';
+      console.log('Block scope i is: ', i);
+    }
+    console.log('Function scope i is: ', i);
+    return i;
+  }
+}
+{
+  const s = [5, 7, 2];
+function editInPlace() {
+  s[0] = 2
+  s[1] = 5
+  s[2] = 7
+  // Only change code below this line
+
+  // Using s = [2, 5, 7] would be invalid
+
+  // Only change code above this line
+}
+editInPlace();
+}
+{
+  function freezeObj() {
+    const MATH_CONSTANTS = {
+      PI: 3.14
+    };
+    // Only change code below this line
+  Object.freeze(MATH_CONSTANTS)
+  
+    // Only change code above this line
+    try {
+      MATH_CONSTANTS.PI = 99;
+    } catch(ex) {
+      console.log(ex);
+    }
+    return MATH_CONSTANTS.PI;
+  }
+  const PI = freezeObj();
+}
