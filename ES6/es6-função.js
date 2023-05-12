962705c489e68d53848ea42dce16cf77d682d363
Priscila <anaandrade7464@gmail.com>
2023-05-12 18:07:27 -0300

    destruturação de array e objeto

--- ES6/es6-função.js
+++ ES6/es6-função.js
@@ -107,4 +107,52 @@ console.log(myConcat([1, 2], [3, 4, 5]));
     // Only change code below this line
        const {today: { low: lowToday , high: highToday }}=LOCAL_FORECAST;
   }
+  let a = 8, b = 6;
+// Only change code below this line
+ [a, b] = [b,a]
+console.log(a,b)
+{
+  function removeFirstTwo(list) {
+  // Only change code below this line
+  const [a, b, ...shorterList] = list; // Change this line
+  // Only change code above this line
+  return shorterList;
+}
+
+const source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const sourceWithoutFirstTwo = removeFirstTwo(source);
+}
+{
+  const stats = {
+  max: 56.78,
+  standard_deviation: 4.34,
+  median: 34.54,
+  mode: 23.87,
+  min: -0.75,
+  average: 35.85
+};
+
+// Only change code below this line
+const half = ({max, min}) => (max + min) / 2.0; 
+}
+//Template Literals
+{
+  const result = {
+    success: ["max-length", "no-amd", "prefer-arrow-functions"],
+    failure: ["no-var", "var-on-top", "linebreak"],
+    skipped: ["no-extra-semi", "no-dup-keys"]
+  };
+  function makeList(arr) {
+    // Only change code below this line
+    const failureItems = [];
+    for(let i = 0; i < arr.length; i++) {
+   failureItems.push(`<li class="text-warning">${arr[i]}</li>`)
+    }
+    // Only change code above this line
+  
+    return failureItems;
+  }
+  
+  const failuresList = makeList(result.failure);
+}
   
