fcec7004e81b11a1f4789fb9f03024b45cb099bb
Priscila <anaandrade7464@gmail.com>
2023-05-25 17:56:13 -0300

    manipulação de string

--- algoritimos/index.js
+++ algoritimos/index.js
@@ -34,3 +34,62 @@
       
       factorialize(5);
 }
+//encontrar a maior palavra de uma string
+{
+  function findLongestWordLength(str) {
+ let strSplit = str.split(' ');
+ let longestWord = 0;
+ for(let i = 0; i < strSplit.length; i++) {
+   if(strSplit[i].length > longestWord){
+     longestWord = strSplit[i].length
+   }
+ }
+  return longestWord;
+}
+
+findLongestWordLength("The quick brown fox jumped over the lazy dog");
+}
+//encontrar o maior numero de um array
+{
+  function largestOfFour(arr) {
+ const results = [];
+ for(let i = 0; i < arr.length; i++){
+   let largestNumber = arr[i][0];
+   for(let j = 1; j < arr[i].length; j++){
+     if(arr[i][j] > largestNumber){
+       largestNumber = arr[i][j];
+     }
+   }
+   results[i] = largestNumber 
+ }
+
+  return results;
+}
+
+largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+}
+//confirmando uma string no final
+{
+  function confirmEnding(str, target) {
+
+  return str.substring(str.length - target.length) === target;
+}
+
+confirmEnding("Bastian", "n");
+}
+//criando uma repetição de string
+{
+  function repeatStringNumTimes(str, num) {
+ let result = ''
+ while(num > 0){
+ console.log('antes', num, result)  
+ result += str;
+ num--
+ console.log('depois', num, result)  
+ } 
+  
+  return result;
+}
+
+repeatStringNumTimes("abc", 3);
+}
