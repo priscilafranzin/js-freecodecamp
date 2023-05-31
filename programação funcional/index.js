77e372ef2bb8b655619776f808cef3f350f8ca0d
Priscila <anaandrade7464@gmail.com>
2023-05-31 17:16:39 -0300

    programação funcional

---
+++ programação funcional/index.js
@@ -0,0 +1,81 @@
+//programação funcional
+{
+    // Function that returns a string representing a cup of green tea
+const prepareTea = () => 'greenTea';
+
+/*
+Given a function (representing the tea type) and number of cups needed, the
+following function returns an array of strings (each representing a cup of
+a specific type of tea).
+*/
+const getTea = (numOfCups) => {
+  const teaCups = [];
+
+  for(let cups = 1; cups <= numOfCups; cups += 1) {
+    const teaCup = prepareTea();
+    teaCups.push(teaCup);
+  }
+  return teaCups;
+};
+
+// Only change code below this line
+const tea4TeamFCC = getTea(40);
+// Only change code above this line
+}
+//função modificada
+{
+    // Function that returns a string representing a cup of green tea
+const prepareGreenTea = () => 'greenTea';
+
+// Function that returns a string representing a cup of black tea
+const prepareBlackTea = () => 'blackTea';
+
+/*
+Given a function (representing the tea type) and number of cups needed, the
+following function returns an array of strings (each representing a cup of
+a specific type of tea).
+*/
+const getTea = (prepareTea, numOfCups) => {
+  const teaCups = [];
+
+  for(let cups = 1; cups <= numOfCups; cups += 1) {
+    const teaCup = prepareTea();
+    teaCups.push(teaCup);
+  }
+  return teaCups;
+};
+
+// Only change code below this line
+const tea4GreenTeamFCC = getTea(prepareGreenTea, 27);
+const tea4BlackTeamFCC = getTea(prepareBlackTea, 13);
+// Only change code above this line
+
+console.log(
+  tea4GreenTeamFCC,
+  tea4BlackTeamFCC
+);
+}
+//variavel global
+{
+    // The global variable
+let fixedValue = 4;
+
+function incrementer() {
+  // Only change code below this line
+return fixedValue +1
+
+  // Only change code above this line
+}
+}
+//argumento da função
+{
+    // The global variable
+let fixedValue = 4;
+
+// Only change code below this line
+function incrementer(nfixedValueun) {
+return fixedValue +1
+
+  // Only change code above this line
+}
+}
