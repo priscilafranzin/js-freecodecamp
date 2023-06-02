a64ea963c9be991dd8dd74618bc1f45d77331a78
Priscila <anaandrade7464@gmail.com>
2023-06-02 16:52:02 -0300

    metodos slice, concat, reduce

--- programação funcional/index.js
+++ programação funcional/index.js
@@ -220,3 +220,120 @@ const filteredList = watchList
 
 console.log(filteredList);
 }
+
+//metodo slice(
+{
+  function sliceArray(anim, beginSlice, endSlice) {
+  // Only change code below this line
+let newArr = anim.slice(beginSlice,endSlice )
+return newArr
+
+
+  // Only change code above this line
+}
+
+const inputAnim = ["Cat", "Dog", "Tiger", "Zebra", "Ant"];
+sliceArray(inputAnim, 1, 3);
+}
+// mudando para metodo slice
+{
+  function nonMutatingSplice(cities) {
+  // Only change code below this line
+  const newArr = cities.slice(0, 3)
+  return newArr
+
+  // Only change code above this line
+}
+
+const inputCities = ["Chicago", "Delhi", "Islamabad", "London", "Berlin"];
+nonMutatingSplice(inputCities);
+}
+//metodo concat
+{
+  function nonMutatingConcat(original, attach) {
+  // Only change code below this line
+return original.concat(attach)
+
+  // Only change code above this line
+}
+
+const first = [1, 2, 3];
+const second = [4, 5];
+nonMutatingConcat(first, second);
+}
+{
+  function nonMutatingPush(original, newItem) {
+  // Only change code below this line
+  return original.concat(newItem);
+
+  // Only change code above this line
+}
+
+const first = [1, 2, 3];
+const second = [4, 5];
+nonMutatingPush(first, second);
+}
+//metodo reduce
+{
+  const watchList = [
+  {
+    "Title": "Inception",
+    "Year": "2010",
+    "Rated": "PG-13",
+    "Released": "16 Jul 2010",
+    "Runtime": "148 min",
+    "Genre": "Action, Adventure, Crime",
+    "Director": "Christopher Nolan",
+    "Writer": "Christopher Nolan",
+    "Actors": "Leonardo DiCaprio, Joseph Gordon-Levitt, Elliot Page, Tom Hardy",
+    "Plot": "A thief, who steals corporate secrets through use of dream-sharing technology, is given the inverse task of planting an idea into the mind of a CEO.",
+    "Language": "English, Japanese, French",
+    "Country": "USA, UK",
+    "Awards": "Won 4 Oscars. Another 143 wins & 198 nominations.",
+    "Poster": "http://ia.media-imdb.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+    "Metascore": "74",
+    "imdbRating": "8.8",
+    "imdbVotes": "1,446,708",
+    "imdbID": "tt1375666",
+    "Type": "movie",
+    "Response": "True"
+  },
+  {
+    "Title": "Interstellar",
+    "Year": "2014",
+    "Rated": "PG-13",
+    "Released": "07 Nov 2014",
+    "Runtime": "169 min",
+    "Genre": "Adventure, Drama, Sci-Fi",
+    "Director": "Christopher Nolan",
+    "Writer": "Jonathan Nolan, Christopher Nolan",
+    "Actors": "Ellen Burstyn, Matthew McConaughey, Mackenzie Foy, John Lithgow",
+    "Plot": "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival.",
+    "Language": "English",
+    "Country": "USA, UK",
+    "Awards": "Won 1 Oscar. Another 39 wins & 132 nominations.",
+    "Poster": "http://ia.media-imdb.com/images/M/MV5BMjIxNTU4MzY4MF5BMl5BanBnXkFtZTgwMzM4ODI3MjE@._V1_SX300.jpg",
+    "Metascore": "74",
+    "imdbRating": "8.6",
+    "imdbVotes": "910,366",
+    "imdbID": "tt0816692",
+    "Type": "movie",
+    "Response": "True"
+  }
+]
+function getRating(watchList) {
+  // Only change code below this line
+  const ratings = watchList
+  .filter((movie) => movie.Director === 'Christopher Nolan')
+  .map((movie) => Number(movie.imdbRating) )
+  let averageRating = ratings
+  .reduce((acc, curr) => acc + curr) / ratings.length
+  console.log(averageRating)
+
+
+  // Only change code above this line
+  return averageRating;
+}
+
+console.log(getRating(watchList));
+}
